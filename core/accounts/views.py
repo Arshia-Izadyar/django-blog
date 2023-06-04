2aa750636938a18ac0e6cf866fe5c0e7dcff4f0a
Arshia izadyar <110552657+arshiaa104@users.noreply.github.com>
2023-06-04 17:44:22 +0330

    added html

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,4 @@
-from typing import Any
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic import FormView
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
@@ -12,20 +10,25 @@ class LoginView(FormView):
     context_object_name = 'form'
     template_name = 'accounts/login.html'
 
-
+    def dispatch(self, request, *args, **kwargs):
+        if request.GET.get('next') is not None:
+            self.success_url = request.GET.get('next')
+        if request.user.is_authenticated is True :
+            return redirect('posts-list')
+        return super().dispatch(request, *args, **kwargs)
     def form_valid(self, form):
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(request=self.request, username=username, password=password)
         if user is not None:
             login(request=self.request, user=user)
+
             return super().form_valid(form)
         else:
             return self.form_invalid(form)
         
 
 class LogoutView(View):
-    # template_name = 'accounts/logout.html'
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             logout(request)
@@ -36,7 +39,8 @@ class SignUpView(FormView):
     form_class = UserCreationForm
     template_name = 'accounts/signup.html'
     def post(self, request, *args, **kwargs):
+        
         form = self.form_class(request.POST)
         form.is_valid()
         form.save()
-        return redirect('posts-list')
+        return redirect('login')
