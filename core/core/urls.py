7e0c500d212f0422079ff07aa05bd8b239ddfead
Arshia izadyar <110552657+Arshia-Izadyar@users.noreply.github.com>
2023-06-06 21:29:42 +0330

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')), 
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('blog.api.v1.urls')),
+    
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
