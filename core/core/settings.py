7c430e44b60bad5898fd2299f44c3c05156d09fc
Arshia izadyar <110552657+arshiaa104@users.noreply.github.com>
2023-06-06 21:39:37 +0330

    added filter and Search field to API

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .local_file import KEY, DATABASE_INFO
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nhn2yqe5mrn_n2kpdlo#2k=2=ae!sj(=%r3j$5q9q6_k=e^$dk'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,17 +78,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "blog",
-        "PASSWORD": "a123",
-        "HOST": "localhost",
-        "USER": "arshia",
-        "PORT": 5432,
-    }
-}
-
+DATABASES = DATABASE_INFO
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
