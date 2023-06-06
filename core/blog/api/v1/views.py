c183690e63f642259055f13aea874dda7ba29b43
Arshia izadyar <110552657+arshiaa104@users.noreply.github.com>
2023-06-06 21:24:22 +0330

    added filters to api

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import Response
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from blog.models import BlogPostModel
 from .serializer import BlogPostSerializer
 from .pagination import BlogPaginator
@@ -11,12 +13,17 @@ class BlogPostListApiView(ListCreateAPIView):
     serializer_class = BlogPostSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
     pagination_class = BlogPaginator
-    # TODO: support multiple pages
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filterset_fields = ('state', )
+    search_fields = ('author__username', 'title')
+    ordering_fields = ('created_date', 'modify_date')
     
+    # filter_queryset = BlogPostModel.objects.aggregate(
 class GetBlogPostApiView(RetrieveUpdateDestroyAPIView):
     queryset = BlogPostModel.objects
     serializer_class = BlogPostSerializer
     permission_classes = (IsAuthenticated, )
+
     
 
     
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
